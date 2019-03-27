d240ed719400b3e86ce38107000f03d64026da51
lightning <aurora.hay@agile6.com>
2019-03-26 17:09:46 -0700

    fix match routes

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Mercury::Engine.routes.draw do
 
   scope '/mercury' do
     get   ':type/:resource',        :to => "mercury#resource"
-    match 'snippets/:name/options', :to => "mercury#snippet_options", :via => [:get, :post]
-    match 'snippets/:name/preview', :to => "mercury#snippet_preview", :via => [:get, :post]
+    match 'snippets/:name/options', :to => "mercury#snippet_options", :via => [:get, :post], :as => 'snippet_options'
+    match 'snippets/:name/preview', :to => "mercury#snippet_preview", :via => [:get, :post], :as => 'preview'
   end
 end
